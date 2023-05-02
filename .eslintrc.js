b184e3e0114cd2621f0afc52e06f151b1fa443f5
kim <yuyukgh@naver.com>
2023-05-03 00:25:18 +0900

    [Feat] eslint 설정 일부 변경 및 gitignore 추가 기본 css 제거

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
         ],
       },
     ],
+    'import/no-unresolved': 'error',
     '@typescript-eslint/no-unsafe-call': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
--- packages/shared-components/src/App.tsx
+++ packages/shared-components/src/App.tsx
@@ -4,10 +4,8 @@ import viteLogo from 'public/vite.svg';
 
 import reactLogo from './assets/react.svg';
 
-import './App.css';
-
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
 
   return (
     <>
